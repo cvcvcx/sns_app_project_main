d2ed436f2257ed8fd280eb2d7b9f2a33069fb609
Cho Changhun <cvcvcx@naver.com>
2022-09-23 20:29:24 +0900

    Initial commit

---
+++ App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ *
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import Greeting from './components/Greeting';
+import Box from './components/Box';
+const App = () => {
+  const name = 'JSX';
+  return (
+    <SafeAreaView>
+      <Greeting name={name} />
+      <Box />
+    </SafeAreaView>
+  );
+};
+export default App;
